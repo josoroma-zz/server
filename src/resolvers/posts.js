f16d40049510b28183b930ece8614a4697445fd3
josoroma <pablo@josoroma.com>
2017-09-11 04:49:32 -0600

    0. First GraphQL request

--- src/resolvers/posts.js
+++ src/resolvers/posts.js
@@ -1,10 +1,30 @@
 export const posts = [
   {
     id: 1,
-    title: 'Post 01'
+    title: 'Turkey t-bone jerky01'
   },
   {
     id: 2,
-    title: 'Post 02'
+    title: 'Pork biltong andouille 02'
+  },
+  {
+    id: 3,
+    title: 'Burgdoggen shankle 03'
+  },
+  {
+    id: 4,
+    title: 'Tongue t-bone leberkas 04'
+  },
+  {
+    id: 5,
+    title: 'Pork flank tenderloin 05'
+  },
+  {
+    id: 6,
+    title: 'Chuck ham capicola 06'
+  },
+  {
+    id: 7,
+    title: 'Pork loin pastrami 07'
   }
 ];
